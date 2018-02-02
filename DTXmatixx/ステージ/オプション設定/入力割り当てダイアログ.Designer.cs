b4ec978d0370dd35f902a709d940852f51bca6df
ＦＲＯＭ <kumakamikoubou@gmail.com>
2018-02-02 15:23:02 +0900

    入力割り当てダイアログの実装を完了。
    結果は、App.システム設定.キーバインディング に保管、ならびにファイルに保存される。

--- DTXmatixx/ステージ/オプション設定/入力割り当てダイアログ.Designer.cs
+++ DTXmatixx/ステージ/オプション設定/入力割り当てダイアログ.Designer.cs
@@ -30,10 +30,13 @@
         {
             this.listView入力リスト = new System.Windows.Forms.ListView();
             this.columnHeaderMIDIノート情報 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.listBox割り当て済み入力リスト = new System.Windows.Forms.ListBox();
             this.buttonOk = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
             this.comboBoxパッドリスト = new System.Windows.Forms.ComboBox();
+            this.button追加 = new System.Windows.Forms.Button();
+            this.button割り当て解除 = new System.Windows.Forms.Button();
+            this.listView割り当て済み入力リスト = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // listView入力リスト
@@ -45,25 +48,16 @@
             this.listView入力リスト.HideSelection = false;
             this.listView入力リスト.LabelWrap = false;
             this.listView入力リスト.Location = new System.Drawing.Point(12, 81);
-            this.listView入力リスト.MultiSelect = false;
             this.listView入力リスト.Name = "listView入力リスト";
-            this.listView入力リスト.Size = new System.Drawing.Size(307, 364);
+            this.listView入力リスト.Size = new System.Drawing.Size(307, 370);
             this.listView入力リスト.TabIndex = 3;
             this.listView入力リスト.UseCompatibleStateImageBehavior = false;
             this.listView入力リスト.View = System.Windows.Forms.View.Details;
+            this.listView入力リスト.DoubleClick += new System.EventHandler(this.listView入力リスト_DoubleClick);
             // 
             // columnHeaderMIDIノート情報
             // 
             this.columnHeaderMIDIノート情報.Width = 300;
-            // 
-            // listBox割り当て済み入力リスト
-            // 
-            this.listBox割り当て済み入力リスト.FormattingEnabled = true;
-            this.listBox割り当て済み入力リスト.ItemHeight = 12;
-            this.listBox割り当て済み入力リスト.Location = new System.Drawing.Point(429, 129);
-            this.listBox割り当て済み入力リスト.Name = "listBox割り当て済み入力リスト";
-            this.listBox割り当て済み入力リスト.Size = new System.Drawing.Size(303, 316);
-            this.listBox割り当て済み入力リスト.TabIndex = 4;
             // 
             // buttonOk
             // 
@@ -89,23 +83,66 @@
             // 
             this.comboBoxパッドリスト.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxパッドリスト.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.comboBoxパッドリスト.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboBoxパッドリスト.Font = new System.Drawing.Font("Meiryo UI", 14F);
             this.comboBoxパッドリスト.FormattingEnabled = true;
             this.comboBoxパッドリスト.Location = new System.Drawing.Point(429, 81);
             this.comboBoxパッドリスト.Name = "comboBoxパッドリスト";
-            this.comboBoxパッドリスト.Size = new System.Drawing.Size(303, 38);
+            this.comboBoxパッドリスト.Size = new System.Drawing.Size(317, 32);
             this.comboBoxパッドリスト.TabIndex = 7;
             this.comboBoxパッドリスト.SelectedIndexChanged += new System.EventHandler(this.comboBoxパッドリスト_SelectedIndexChanged);
+            // 
+            // button追加
+            // 
+            this.button追加.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.button追加.Location = new System.Drawing.Point(326, 208);
+            this.button追加.Name = "button追加";
+            this.button追加.Size = new System.Drawing.Size(97, 59);
+            this.button追加.TabIndex = 8;
+            this.button追加.Text = "→\r\n追加する";
+            this.button追加.UseVisualStyleBackColor = true;
+            this.button追加.Click += new System.EventHandler(this.button追加_Click);
+            // 
+            // button割り当て解除
+            // 
+            this.button割り当て解除.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.button割り当て解除.Location = new System.Drawing.Point(326, 296);
+            this.button割り当て解除.Name = "button割り当て解除";
+            this.button割り当て解除.Size = new System.Drawing.Size(97, 59);
+            this.button割り当て解除.TabIndex = 10;
+            this.button割り当て解除.Text = "×←\r\n削除する";
+            this.button割り当て解除.UseVisualStyleBackColor = true;
+            this.button割り当て解除.Click += new System.EventHandler(this.button割り当て解除_Click);
+            // 
+            // listView割り当て済み入力リスト
+            // 
+            this.listView割り当て済み入力リスト.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView割り当て済み入力リスト.FullRowSelect = true;
+            this.listView割り当て済み入力リスト.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView割り当て済み入力リスト.HideSelection = false;
+            this.listView割り当て済み入力リスト.LabelWrap = false;
+            this.listView割り当て済み入力リスト.Location = new System.Drawing.Point(429, 131);
+            this.listView割り当て済み入力リスト.Name = "listView割り当て済み入力リスト";
+            this.listView割り当て済み入力リスト.Size = new System.Drawing.Size(317, 320);
+            this.listView割り当て済み入力リスト.TabIndex = 11;
+            this.listView割り当て済み入力リスト.UseCompatibleStateImageBehavior = false;
+            this.listView割り当て済み入力リスト.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 300;
             // 
             // 入力割り当てダイアログ
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(744, 511);
+            this.ClientSize = new System.Drawing.Size(763, 511);
+            this.Controls.Add(this.listView割り当て済み入力リスト);
+            this.Controls.Add(this.button割り当て解除);
+            this.Controls.Add(this.button追加);
             this.Controls.Add(this.comboBoxパッドリスト);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOk);
-            this.Controls.Add(this.listBox割り当て済み入力リスト);
             this.Controls.Add(this.listView入力リスト);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -116,6 +153,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "入力割り当て";
             this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.入力割り当てダイアログ_FormClosing);
             this.ResumeLayout(false);
 
         }
@@ -123,10 +161,13 @@
         #endregion
 
         private System.Windows.Forms.ListView listView入力リスト;
-        private System.Windows.Forms.ListBox listBox割り当て済み入力リスト;
         private System.Windows.Forms.Button buttonOk;
         private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.ColumnHeader columnHeaderMIDIノート情報;
         private System.Windows.Forms.ComboBox comboBoxパッドリスト;
+        private System.Windows.Forms.Button button追加;
+        private System.Windows.Forms.Button button割り当て解除;
+        private System.Windows.Forms.ListView listView割り当て済み入力リスト;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
     }
 }
--- DTXmatixx/ステージ/オプション設定/入力割り当てダイアログ.cs
+++ DTXmatixx/ステージ/オプション設定/入力割り当てダイアログ.cs
@@ -7,6 +7,7 @@ using SharpDX.DirectInput;
 using FDK;
 using FDK.メディア;
 using FDK.入力;
+using FDK.カウンタ;
 using DTXmatixx.設定;
 using DTXmatixx.入力;
 
@@ -23,78 +24,301 @@ namespace DTXmatixx.ステージ.オプション設定
                 new System.Drawing.SizeF(
                     Math.Max( 1f, グラフィックデバイス.Instance.拡大率DPXtoPX横 ),
                     Math.Max( 1f, グラフィックデバイス.Instance.拡大率DPXtoPX縦 ) ) );
-
-            // パッドリストを初期化。
-            foreach( ドラム入力種別 drum in Enum.GetValues( typeof( ドラム入力種別 ) ) )
-            {
-                if( drum != ドラム入力種別.Unknown )   // Unknown 以外を登録。
-                    this.comboBoxパッドリスト.Items.Add( drum.ToString() );
-            }
-
-            // 変更後のキーバインディングを初期化。
-            this._変更後のキーバインディング = (キーバインディング) App.システム設定.キーバインディング.Clone();
-
-            // 最初のパッドを選択し、割り当て済みリストを更新。
-            this.comboBoxパッドリスト.SelectedIndex = 0;
         }
-        public DialogResult 表示する()
+
+        public void 表示する()
         {
-            // このウィンドウ用の入力管理インスタンスを生成。
-            using( var 入力管理 = new 入力.入力管理( this.Handle ) )
+            using( var 入力管理 = new 入力.入力管理( this.Handle ) )  // このウィンドウ用の入力管理インスタンスを生成。
             using( var timer = new Timer() )
             {
+                #region " 初期化。"
+                //----------------
                 入力管理.Initialize();
 
+                // パッドリストを初期化。
+                foreach( ドラム入力種別 drum in Enum.GetValues( typeof( ドラム入力種別 ) ) )
+                {
+                    if( drum != ドラム入力種別.Unknown )   // Unknown 以外を登録。
+                        this.comboBoxパッドリスト.Items.Add( drum.ToString() );
+                }
+
+                // 変更後のキーバインディングを、現在の設定値で初期化。
+                this._変更後のキーバインディング = (キーバインディング) App.システム設定.キーバインディング.Clone();
+
+                // 最初のパッドを選択し、割り当て済みリストを更新。
+                this.comboBoxパッドリスト.SelectedIndex = 0;
+
+                this._前回の入力リスト追加時刻 = QPCTimer.生カウント相対値を秒へ変換して返す( QPCTimer.生カウント );
+
+                this._変更あり = false;
+                //----------------
+                #endregion
+
+                #region " タイマーイベント処理。"
+                //----------------
                 timer.Interval = 100;
                 timer.Tick += ( sender, arg ) => {
-                    this._デバイスをポーリングして表示する( 入力管理.Keyboard, ( ie ) => $"Keyboard, {ie.Key}, '{( (Key) ie.Key ).ToString()}'" );
-                    this._デバイスをポーリングして表示する( 入力管理.MidiIn, ( ie ) => $"MidiIn[{ie.DeviceID}], Note:{ie.Key}, Velocity:{ie.Velocity}" );
+
+                    // (A) キーボードのポーリングと、入力リストへの出力。
+
+                    入力管理.Keyboard.ポーリングする();
+
+                    for( int i = 0; i < 入力管理.Keyboard.入力イベントリスト.Count; i++ )
+                    {
+                        var ie = 入力管理.Keyboard.入力イベントリスト[ i ];
+
+                        if( ie.押された )
+                        {
+                            var item = new ListViewItem入力リスト用( InputDeviceType.Keyboard, ie );
+
+                            this._一定時間が経っていれば空行を挿入する();
+
+                            this.listView入力リスト.Items.Add( item );
+                            this.listView入力リスト.EnsureVisible( this.listView入力リスト.Items.Count - 1 );
+                        }
+                    }
+
+                    // (B) Midi入力のポーリングと、入力リストへの出力。
+
+                    入力管理.MidiIn.ポーリングする();
+
+                    for( int i = 0; i < 入力管理.MidiIn.入力イベントリスト.Count; i++ )
+                    {
+                        var ie = 入力管理.MidiIn.入力イベントリスト[ i ];
+
+                        if( ie.押された )
+                        {
+                            var item = new ListViewItem入力リスト用( InputDeviceType.MidiIn, ie );
+
+                            this._一定時間が経っていれば空行を挿入する();
+
+                            this.listView入力リスト.Items.Add( item );
+                            this.listView入力リスト.EnsureVisible( this.listView入力リスト.Items.Count - 1 );
+                        }
+                    }
+
                 };
+                //----------------
+                #endregion
 
+                DialogResult dr;
+
+                #region " ダイアログ表示→終了。"
+                //----------------
                 timer.Start();
-                var dr = this.ShowDialog( App.Instance );
+
+                dr = this.ShowDialog( App.Instance );
+
                 timer.Stop();
+                //----------------
+                #endregion
 
-                return dr;
-            }
-        }
-
-        private キーバインディング _変更後のキーバインディング;
-
-        private void _割り当て済みリストを更新する()
-        {
-            var 選択されているパッド = (ドラム入力種別) Enum.Parse( typeof( ドラム入力種別 ), (string) this.comboBoxパッドリスト.SelectedItem );
-
-            this.listBox割り当て済み入力リスト.Items.Clear();
-
-            var キー割り当て = this._変更後のキーバインディング.キーボードtoドラム.Where( ( kvp ) => ( kvp.Value == 選択されているパッド ) );
-            foreach( var key in キー割り当て )
-                this.listBox割り当て済み入力リスト.Items.Add( $"Keyboard, {key.Key.key}, '{( (Key) key.Key.key ).ToString()}'" );
-
-            var MidiIn割り当て = this._変更後のキーバインディング.MIDItoドラム.Where( ( kvp ) => ( kvp.Value == 選択されているパッド ) );
-            foreach( var note in MidiIn割り当て )
-                this.listBox割り当て済み入力リスト.Items.Add( $"MidiIn[{note.Key.deviceId}], Note:{note.Key.key}" );
-        }
-        private void _デバイスをポーリングして表示する( IInputDevice device, Func<InputEvent, string> 行を取得 )
-        {
-            device.ポーリングする();
-
-            for( int i = 0; i < device.入力イベントリスト.Count; i++ )
-            {
-                var ie = device.入力イベントリスト[ i ];
-
-                if( ie.押された )
+                if( dr == DialogResult.OK )
                 {
-                    this.listView入力リスト.Items.Add( 行を取得( ie ) );
-                    this.listView入力リスト.EnsureVisible( this.listView入力リスト.Items.Count - 1 );
+                    #region " 設定値の反映。"
+                    //----------------
+                    App.システム設定.キーバインディング = (キーバインディング) this._変更後のキーバインディング.Clone();
+                    App.システム設定.保存する();
+                    //----------------
+                    #endregion
                 }
             }
+        }
 
+        private class ListViewItem入力リスト用 : ListViewItem
+        {
+            public InputDeviceType deviceType;  // Device種別
+            public InputEvent inputEvent;       // DeviceID, key, velocity
+
+            public ListViewItem入力リスト用( InputDeviceType deviceType, InputEvent inputEvent )
+            {
+                this.deviceType = deviceType;
+                this.inputEvent = inputEvent;
+
+                this.Text = 
+                    ( deviceType == InputDeviceType.Keyboard ) ? $"Keyboard, {inputEvent.Key}, '{( (Key) inputEvent.Key ).ToString()}'" :
+                    ( deviceType == InputDeviceType.MidiIn ) ? $"MidiIn[{inputEvent.DeviceID}], Note:{inputEvent.Key}, Velocity:{inputEvent.Velocity}" :
+                    throw new ArgumentException( "未対応のデバイスです。" );
+            }
+        }
+        private class ListViewItem割り当て済み入力リスト用 : ListViewItem
+        {
+            public InputDeviceType deviceType;      // Device種別
+            public キーバインディング.IdKey idKey;   // DeviceID, key
+
+            public ListViewItem割り当て済み入力リスト用( InputDeviceType deviceType, キーバインディング.IdKey idKey )
+            {
+                this.deviceType = deviceType;
+                this.idKey = idKey;
+
+                this.Text = 
+                    ( deviceType == InputDeviceType.Keyboard ) ? $"Keyboard, {idKey.key}, '{( (Key) idKey.key ).ToString()}'" :
+                    ( deviceType == InputDeviceType.MidiIn ) ? $"MidiIn[{idKey.deviceId}], Note:{idKey.key}" :
+                    throw new ArgumentException( "未対応のデバイスです。" );
+            }
+        }
+        private キーバインディング _変更後のキーバインディング;
+        private bool _変更あり;
+        private double _前回の入力リスト追加時刻;
+        private ドラム入力種別 _現在選択されているドラム入力種別 = ドラム入力種別.Unknown;
+
+        /// <summary>
+        ///     <see cref="_現在選択されているドラム入力種別"/> について、<see cref="_変更後のキーバインディング"/> の内容を、
+        ///     割り当て済みリストに反映する。
+        /// </summary>
+        private void _割り当て済みリストを更新する( ListViewItem入力リスト用 選択する項目 = null )
+        {
+            this.listView割り当て済み入力リスト.Items.Clear();
+
+            // (A) キーボード
+
+            var キー割り当て = this._変更後のキーバインディング
+                .キーボードtoドラム
+                .Where( ( kvp ) => ( kvp.Value == this._現在選択されているドラム入力種別 ) );
+
+            foreach( var key in キー割り当て )
+                this.listView割り当て済み入力リスト.Items.Add( new ListViewItem割り当て済み入力リスト用( InputDeviceType.Keyboard, key.Key ) );
+
+            // (B) MIDI入力
+
+            var MidiIn割り当て = this._変更後のキーバインディング
+                .MIDItoドラム
+                .Where( ( kvp ) => ( kvp.Value == this._現在選択されているドラム入力種別 ) );
+
+            foreach( var note in MidiIn割り当て )
+                this.listView割り当て済み入力リスト.Items.Add( new ListViewItem割り当て済み入力リスト用( InputDeviceType.MidiIn, note.Key ) );
+
+            // フォーカス
+
+            if( null != 選択する項目 )
+            {
+                foreach( ListViewItem割り当て済み入力リスト用 item in this.listView割り当て済み入力リスト.Items )
+                {
+                    if( item.deviceType == 選択する項目.deviceType &&
+                        item.idKey.deviceId == 選択する項目.inputEvent.DeviceID &&
+                        item.idKey.key == 選択する項目.inputEvent.Key )
+                    {
+                        // > 項目をプログラムで選択しても、フォーカスは自動的に ListView コントロールには変更されません。
+                        // > そのため、項目を選択するときは、通常、その項目をフォーカスがある状態に設定します。（MSDNより）
+                        // https://msdn.microsoft.com/ja-jp/library/y4x56c0b(v=vs.110).aspx
+                        item.Focused = true;
+                        item.Selected = true;
+                    }
+                }
+            }
+        }
+        private void _現在選択されている入力リストの入力行を割り当て済み入力リストに追加する()
+        {
+            foreach( var itemobj in this.listView入力リスト.SelectedItems )
+            {
+                // 選択されているのが ListViewItem入力リスト用 じゃなければ何もしない。
+                if( itemobj is ListViewItem入力リスト用 item )
+                {
+                    var idKey = new キーバインディング.IdKey( item.inputEvent );
+
+                    // (A) キーボード
+                    if( item.deviceType == InputDeviceType.Keyboard )
+                    {
+                        // すでに割り当て済みの場合は、先にそれを削除する。
+                        this._変更後のキーバインディング
+                            .キーボードtoドラム
+                            .Remove( idKey );
+
+                        // 追加し、更新。
+                        this._変更後のキーバインディング
+                            .キーボードtoドラム
+                            .Add( idKey, this._現在選択されているドラム入力種別 );
+
+                        this._割り当て済みリストを更新する( item );
+                        this.listView割り当て済み入力リスト.Focus();
+                        this._変更あり = true;
+                    }
+
+                    // (B) Midi入力
+                    else if( item.deviceType == InputDeviceType.MidiIn )
+                    {
+                        // すでに割り当て済みの場合は、先にそれを削除する。
+                        this._変更後のキーバインディング
+                            .MIDItoドラム
+                            .Remove( idKey );
+
+                        // 追加し、更新。
+                        this._変更後のキーバインディング
+                            .MIDItoドラム
+                            .Add( idKey, this._現在選択されているドラム入力種別 );
+
+                        this._割り当て済みリストを更新する( item );
+                        this.listView割り当て済み入力リスト.Focus();
+                        this._変更あり = true;
+                    }
+                }
+            }
+        }
+        private void _一定時間が経っていれば空行を挿入する()
+        {
+            double 今回の入力リスト追加時刻 = QPCTimer.生カウント相対値を秒へ変換して返す( QPCTimer.生カウント );
+
+            if( 1.0 < ( 今回の入力リスト追加時刻 - this._前回の入力リスト追加時刻 ) )   // 1秒以上経っていれば
+                this.listView入力リスト.Items.Add( "" );
+
+            this._前回の入力リスト追加時刻 = 今回の入力リスト追加時刻;
         }
 
         private void comboBoxパッドリスト_SelectedIndexChanged( object sender, EventArgs e )
         {
+            this._現在選択されているドラム入力種別 = (ドラム入力種別) Enum.Parse( typeof( ドラム入力種別 ), (string) this.comboBoxパッドリスト.SelectedItem );
+
             this._割り当て済みリストを更新する();
+        }
+        private void button追加_Click( object sender, EventArgs e )
+        {
+            this._現在選択されている入力リストの入力行を割り当て済み入力リストに追加する();
+        }
+        private void listView入力リスト_DoubleClick( object sender, EventArgs e )
+        {
+            this._現在選択されている入力リストの入力行を割り当て済み入力リストに追加する();
+        }
+        private void button割り当て解除_Click( object sender, EventArgs e )
+        {
+            // 選択されている項目に対応する入力をキーバインディングから削除。
+            foreach( ListViewItem割り当て済み入力リスト用 item in this.listView割り当て済み入力リスト.SelectedItems )
+            {
+                // (A) キーボード。
+                if( item.deviceType == InputDeviceType.Keyboard )
+                {
+                    // 割り当て済みの場合は、削除する。
+                    this._変更後のキーバインディング
+                        .キーボードtoドラム
+                        .Remove( item.idKey );
+
+                    this._変更あり = true;
+                }
+
+                // (B) MIDI入力。
+                else if( item.deviceType == InputDeviceType.MidiIn )
+                {
+                    // 割り当て済みの場合は、削除する。
+                    this._変更後のキーバインディング
+                        .MIDItoドラム
+                        .Remove( item.idKey );
+
+                    this._変更あり = true;
+                }
+            }
+
+            this._割り当て済みリストを更新する();
+        }
+
+        private void 入力割り当てダイアログ_FormClosing( object sender, FormClosingEventArgs e )
+        {
+            // ※ウィンドウを閉じようとした時も Cancel になる。
+            if( this.DialogResult == DialogResult.Cancel && this._変更あり )
+            {
+                var dr = MessageBox.Show( "変更を破棄していいですか？", "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2 );
+
+                if( dr == DialogResult.No )
+                    e.Cancel = true;
+            }
         }
     }
 }
