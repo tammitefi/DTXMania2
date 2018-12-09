22c1bb6ca8579f34a0551f4f02b72f44b5054858
ＦＲＯＭ <kumakamikoubou@gmail.com>
2018-12-09 13:15:04 +0900

    IDTXManiaService 実装部分を分離。

---
+++ DTXMania/App.IDTXManiaService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace DTXMania
+{
+    /// <summary>
+    ///		IDTXManiaService の実装
+    /// </summary>
+    /// <remarks>
+    ///		このアセンブリ（exe）は、WCF で IDTXManiaService を公開する。
+    ///		このサービスインターフェースは、シングルスレッド（GUIスレッド）で同期実行される。（Appクラスの ServiceBehavior属性を参照。）
+    ///		このサービスホストはシングルトンであり、すべてのクライアントセッションは同一（単一）のサービスインスタンスへ接続される。（Program.Main() を参照。）
+    /// </remarks>
+    partial class App : DTXMania.Viewer.IDTXManiaService
+    {
+        /// <summary>
+        ///		曲を読み込み、演奏を開始する。
+        ///		ビュアーモードのときのみ有効。
+        /// </summary>
+        /// <param name="path">曲ファイルパス</param>
+        /// <param name="startPart">演奏開始小節番号(0～)</param>
+        /// <param name="drumsSound">ドラムチップ音を発声させるなら true。</param>
+        public void ViewerPlay( string path, int startPart = 0, bool drumsSound = true )
+        {
+            // TODO: ViewerPlay メソッドを実装する。
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///		現在の演奏を停止する。
+        ///		ビュアーモードのときのみ有効。
+        /// </summary>
+        public void ViewerStop()
+        {
+            // TODO: ViewerStop メソッドを実装する。
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///		サウンドデバイスの発声遅延[ms]を返す。
+        /// </summary>
+        /// <returns>遅延量[ms]</returns>
+        public float GetSoundDelay()
+            => (float) ( App.サウンドデバイス?.再生遅延sec ?? 0.0 ) * 1000.0f;
+    }
+}
--- DTXMania/App.cs
+++ DTXMania/App.cs
@@ -23,7 +23,7 @@ using DTXMania.Viewer;
 namespace DTXMania
 {
     [ServiceBehavior( InstanceContextMode = InstanceContextMode.Single )]   // サービスインターフェースをシングルスレッドで呼び出す。
-    class App : ApplicationForm, IDTXManiaService
+    partial class App : ApplicationForm
     {
         public static int リリース番号
             => int.TryParse( Application.ProductVersion.Split( '.' ).ElementAt( 0 ), out int release ) ? release : throw new Exception( "アセンブリのプロダクトバージョンに記載ミスがあります。" );
@@ -263,44 +263,6 @@ namespace DTXMania
 
 			base.OnDeactivate( e );
 		}
-
-		#region " IDTXManiaService の実装 "
-		//----------------
-		// このアセンブリ（exe）は、WCF で IDTXManiaService を公開する。
-		// ・このサービスインターフェースは、シングルスレッド（GUIスレッド）で同期実行される。（Appクラスの ServiceBehavior属性を参照。）
-		// ・このサービスホストはシングルトンであり、すべてのクライアントセッションは同一（単一）のサービスインスタンスへ接続される。（Program.Main() を参照。）
-
-		/// <summary>
-		///		曲を読み込み、演奏を開始する。
-		///		ビュアーモードのときのみ有効。
-		/// </summary>
-		/// <param name="path">曲ファイルパス</param>
-		/// <param name="startPart">演奏開始小節番号(0～)</param>
-		/// <param name="drumsSound">ドラムチップ音を発声させるなら true。</param>
-		public void ViewerPlay( string path, int startPart = 0, bool drumsSound = true )
-        {
-            // TODO: ViewerPlay メソッドを実装する。
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        ///		現在の演奏を停止する。
-        ///		ビュアーモードのときのみ有効。
-        /// </summary>
-        public void ViewerStop()
-        {
-            // TODO: ViewerStop メソッドを実装する。
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        ///		サウンドデバイスの発声遅延[ms]を返す。
-        /// </summary>
-        /// <returns>遅延量[ms]</returns>
-        public float GetSoundDelay()
-            => (float) ( App.サウンドデバイス?.再生遅延sec ?? 0.0 ) * 1000.0f;
-        //----------------
-        #endregion
 
 
         // ※ Form イベントの override メソッドは描画スレッドで実行されるため、処理中に進行タスクが呼び出されると困る場合には、進行タスクとの lock を忘れないこと。
